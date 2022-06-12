55a1b32c230e20cf808a2ffb0924a6a3378c2997
Bao Do <55795855+baodv1001@users.noreply.github.com>
2022-06-12 10:54:49 +0700

    Add UI splash and Logo

--- app/src/main/java/com/example/FruitLearning/SplashActivity.java
+++ app/src/main/java/com/example/FruitLearning/SplashActivity.java
@@ -3,14 +3,21 @@ package com.example.FruitLearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView logo_imgv;
+    ImageView kid_imgv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,24 @@ public class SplashActivity extends AppCompatActivity {
                 nextActivity();
             }
         }, 3000);
+
+        logo_imgv = findViewById(R.id.logo);
+        kid_imgv = findViewById(R.id.kid);
+        try {
+            // get input stream
+
+            InputStream ims = getAssets().open("images/Logo.png");
+            InputStream ims1 = getAssets().open("images/kid.png");
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            Drawable d1 = Drawable.createFromStream(ims1, null);
+            // set image to ImageView
+            logo_imgv.setImageDrawable(d);
+            kid_imgv.setImageDrawable(d1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void nextActivity() {
